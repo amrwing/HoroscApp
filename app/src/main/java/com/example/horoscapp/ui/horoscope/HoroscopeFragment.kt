ce1c57e8b892fc30b0265bea1d0abbc490c832f8
amrwing <amrwing@krasamo.com>
2025-01-16 13:51:30 -0600

    added dependecies injection

--- app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,15 +1,65 @@
 package com.example.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.horoscapp.databinding.FragmentHoroscopeBinding
+import com.example.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeadapter:HoroscopeAdapter
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initList()
+        initUIState()
+    }
+
+    private fun initList() {
+        horoscopeadapter = HoroscopeAdapter(onItemSelected = {Toast.makeText(context,getString(it.name),Toast.LENGTH_LONG).show()})
+        binding.rvHoroscopes.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = horoscopeadapter
+        }
+        binding.rvHoroscopes.layoutManager = GridLayoutManager(context,2)
+        binding.rvHoroscopes.adapter = horoscopeadapter
+    }
+
+    private fun initUIState() {
+        //This attaches the Fragment with the viewmodel
+        //NEEDED
+        //StateFlow in the viewmodel
+        //On the init function, we assign the value
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeViewModel.horoscope.collect{
+                   horoscopeadapter.updateList(it)
+                }
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
