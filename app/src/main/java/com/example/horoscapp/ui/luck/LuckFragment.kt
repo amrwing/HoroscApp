705e813c9ea50c3cd857bf3f86d35e1ecce502dd
Antonio Moreno <59669113+amrwing@users.noreply.github.com>
2025-01-17 16:58:03 -0600

    Swipe modified

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -2,6 +2,7 @@ package com.example.horoscapp.ui.luck
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -17,6 +18,7 @@ import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.FragmentLuckBinding
+import com.example.horoscapp.ui.core.listeners.OnSwipeTouchListener
 import com.example.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
@@ -70,10 +72,18 @@ val sendIntent:Intent = Intent().apply {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener{
-            spinRoulette()
-        }
+        binding.ivRoulette.setOnTouchListener(object  : OnSwipeTouchListener(requireContext()){
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+
+        })
     }
 
     private fun spinRoulette() {
